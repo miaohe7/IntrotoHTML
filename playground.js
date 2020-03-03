4ff2c61de1319dbbae670f7b9db56e4ccb2377d0
miaohe7 <49568139+miaohe7@users.noreply.github.com>
2020-03-03 00:36:18 -0500

    add calendar

--- playground.js
+++ playground.js
@@ -1,32 +1,10 @@
-function welcomeMsg(){
-	alert("Welcome to JavaScript!")
-}
-function welcome(msg){
-	alert(msg);
+var fruits = ["Apple","Banana","Grapes"];
+function loadFruits(){
+	document.getElementById('fruits').innerHTML = fruits;
 }
 
-// var x = "Hello";
-// welcome(x);
-// x = "Goodbye";
-// welcome(x);
-
-function welcomeMsg(Msg){
-	alert(msg);
-	var name = prompt("What is your name?");
-	return name;
-}
-
-//var firstName = welcomeMsg("Hi");
-
-// function message(){
-// 			alert("This alert box was called with the onload message");
-// 		}
-
-
-function message(msg){
-	document.getElementById('output').innerHTML = msg +" event";
-}
-
-function displayDate(){
-	document.getElementById("demo").innerHTML = Date();
+function myFunction(){
+	var fruit = prompt("What is your favourite fruit?");
+	fruits[fruits.length] = fruit;
+	document.getElementById("fruits").innerHTML = fruits;
 }
