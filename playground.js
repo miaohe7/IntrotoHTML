dc5ca891a8d4a93d5de1735bf958b00fe6c0e63a
miaohe7 <49568139+miaohe7@users.noreply.github.com>
2020-03-05 16:28:53 -0500

    responsive changes

--- playground.js
+++ playground.js
@@ -8,3 +8,12 @@ function myFunction(){
 	fruits[fruits.length] = fruit;
 	document.getElementById("fruits").innerHTML = fruits;
 }
+
+function check(){
+	var email1 = document.getElementById('email_addr');
+	var email2 = document.getElementById('email_repeat');
+	if(email1.value != email2.value){
+		alert("The two emails must match!");
+		return false;
+	}
+}
